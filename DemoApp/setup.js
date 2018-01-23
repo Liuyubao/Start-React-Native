750cb7eb4e66f5c1180f1cdc7aa4236079f3c2a5
Liuyubao <523331232@qq.com>
2018-01-23 16:47:05 +0800

    upload all

---
+++ DemoApp/setup.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import HelloComponent from './HelloComponent'
+
+export default class setup extends Component {
+    render() {
+        return (
+           <View style={styles.container}>
+               <HelloComponent
+                   name = "小明"
+               />
+           </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop:150,
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
